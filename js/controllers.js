5df44a83ddadedd7ee5c2f6928977c63a6273e3c
elialbert <eli@simplerelevance.com>
2015-01-04 16:30:12 -0600

    fixed bug in manual mode

--- js/controllers.js
+++ js/controllers.js
@@ -94,7 +94,7 @@ cpmodule
 	$scope.manualMode = 'false';
 	$scope.roomName = roomName;
 	$scope.fridgeName = '';
-	$scope.wordListText = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversations?' So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her."
+	$scope.wordListText = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversations?' So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her.";
 
 	$scope.testValidation = function() {
 	    if ($scope.newfridge.fridgenameinput.$invalid) {
@@ -117,7 +117,7 @@ cpmodule
 		else {
 		    $scope.status = "";
 		    if ($scope.manualMode == 'true') {
-			var wordsToUse = $scope.wordListText;
+			var wordsToUse = $scope.wordListText ;//|| backupmanual;
 		    }
 		    else {
 			var wordsToUse = $scope.selectedPremade.t;
